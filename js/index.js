1452431e4818bb6cdc746d9acfca831d30989737
jingjing0607 <wq930607@126.com>
2020-06-20 14:58:34 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var arrow_left=document.getElementById('arrow-left');
+    right=document.getElementById('arrow-right');
+    imgs=document.getElementById('banner-inner');
+
+left.onclick=function(){
+
+}
+right.onclick=function(){
+
+}
